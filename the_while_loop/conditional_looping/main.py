e9a01e8d5c680796d2f0065fe5e6e4215a2e0487
temizkankubra40-hub <>
2026-04-23 14:57:24 +0000

    Conditional Looping

---
+++ the_while_loop/conditional_looping/main.py
@@ -0,0 +1,14 @@
+# List of country names
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# Counter variable
+short_name_count = 0
+i = 0
+
+while i < len(countries):
+    if len(countries[i]) < 7:
+        short_name_count += 1
+    i += 1
+
+# Testing
+print('Number of short country names:', short_name_count)
